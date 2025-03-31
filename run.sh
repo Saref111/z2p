3d4825e3b29131a9149f4d6f6825c84fbfca52b9
Ivan Borovyk <saref012@gmail.com>
2025-03-31 04:09:49 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
-#! usr/bin/bash
+#!/usr/bin/env bash
+command=$1
 
-cargo watch -x check -x test -x run
+# If command is empty
+if [ -z "$command" ]
+then
+    cargo watch -x check -x test -x run
+elif [ "$command" == "coverage" ]
+then
+    cargo tarpaulin --ignore-tests
+elif [ "$command" == "fmt" ]
+then
+    cargo fmt
+fi
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
-use actix_web::{App, HttpServer, Responder, web};
+use actix_web::{App, HttpRequest, HttpServer, Responder, web};
 
-async fn hello() -> impl Responder {
-    "Hello world!"
+async fn hello(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {name}!")
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(hello)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+            .route("/{name}", web::get().to(hello))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
