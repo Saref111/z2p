7795423702fab2c88799e3796d49030d03c8ef6e
Ivan Borovyk <saref012@gmail.com>
2025-04-01 17:15:35 +0300

    add tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,18 @@
 use std::net::TcpListener;
 
 use reqwest;
+use sqlx::{Connection, PgConnection};
+use z2p::configuration::get_configuration;
+
+pub fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = z2p::startup::run(listener).expect("Failed to bind address.");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{port}")
+}
 
 #[tokio::test]
 async fn health_check_works() {
@@ -18,19 +30,16 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
-    let port = listener.local_addr().unwrap().port();
-    let server = z2p::startup::run(listener).expect("Failed to bind address.");
-
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{port}")
-}
-
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
     let app_address = spawn_app();
+
+    let config = get_configuration().expect("Failed to read config");
+    let connection_string = config.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres");
+
     let client = reqwest::Client::new();
 
     let body = "username=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -43,6 +52,11 @@ async fn subscribe_returns_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT name, email FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
 }
 
 #[tokio::test]
