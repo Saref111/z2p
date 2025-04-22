de1fcc5b92c75ae67e9e4e101aa101246abe2acb
Ivan Borovyk <saref012@gmail.com>
2025-04-22 14:23:44 +0300

    updated dependencies and add logger

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use z2p::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", config.app_port);
     let connection_pool = PgPool::connect(&config.database.connection_string())
