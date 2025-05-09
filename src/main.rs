2e22474663297c1a2cdf1e4e11364c2600b7d51f
Ivan Borovyk <saref012@gmail.com>
2025-05-09 13:21:02 +0300

    beautify logs output for testing

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ use z2p::{
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber(
+        "zero2prod".into(), 
+        "info".into(),
+        std::io::stdout
+    );
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration");
