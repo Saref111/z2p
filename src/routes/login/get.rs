0c521bf2b162dd288dfcd70440e03ec8fa3ee060
Ivan Borovyk <saref012@gmail.com>
2025-09-03 13:23:45 +0300

    add login error message

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,7 +1,25 @@
-use actix_web::{HttpResponse, http::header::ContentType};
+use actix_web::{HttpResponse, http::header::ContentType, web};
+use tera::{self, Context as TeraContext};
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>,
+}
+
+pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+    let error = match query.0.error {
+        Some(e) => e,
+        None => "".into(),
+    };
+
+    let mut ctx = TeraContext::new();
+    ctx.insert("error", &error);
+    let tera = tera::Tera::new("views/**/*").expect("Failed to initialize Tera templates");
+    let page_string = tera
+        .render("login.html", &ctx)
+        .expect("Failed rendering login page.");
+
     HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("../../../views/login.html"))
+        .body(page_string)
 }
