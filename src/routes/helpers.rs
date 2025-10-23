913bfe45590a522c10748f11b1631023d3e0706d
Ivan Borovyk <saref012@gmail.com>
2025-10-23 10:08:00 +0300

    add dashboard page template and refactore

--- src/routes/helpers.rs
+++ src/routes/helpers.rs
@@ -11,3 +11,13 @@ pub fn error_chain_fmt(e: &impl Error, f: &mut std::fmt::Formatter) -> std::fmt:
 
     Ok(())
 }
+
+pub fn prepare_html_template(entries: &[(&str, &str)], template_name: &str) -> String {
+    let mut ctx = tera::Context::new();
+    for (key, value) in entries.to_vec() {
+        ctx.insert(key, value);
+    }
+    let tera = tera::Tera::new("views/**/*").expect("Failed to initialize Tera templates");
+    tera.render(template_name, &ctx)
+        .expect("Failed rendering email template")
+}
--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,7 +1,7 @@
+use crate::routes::helpers::prepare_html_template;
 use actix_web::{HttpResponse, cookie::Cookie, http::header::ContentType};
 use actix_web_flash_messages::{IncomingFlashMessages, Level};
 use std::fmt::Write;
-use tera::{self, Context as TeraContext};
 
 pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
     let mut error_string = String::new();
@@ -9,12 +9,7 @@ pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
         writeln!(error_string, "{}", m.content()).unwrap();
     }
 
-    let mut ctx = TeraContext::new();
-    ctx.insert("error", &error_string);
-    let tera = tera::Tera::new("views/**/*").expect("Failed to initialize Tera templates");
-    let page_string = tera
-        .render("login.html", &ctx)
-        .expect("Failed rendering login page.");
+    let page_string = prepare_html_template(&[("error", &error_string)], "login.html");
 
     let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
